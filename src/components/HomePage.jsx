746b131ac302d8110acf4f7956f8fd05550c72f4
ibrahimnet <joseitr2007@gmail.com>
2025-08-27 16:08:40 -0400

    Esqueleto del Home Page

---
+++ src/components/HomePage.jsx
@@ -0,0 +1,29 @@
+import  react from "react"
+
+function App() {
+
+  return (
+    <>
+      <header>
+        <nav>
+            <div>
+                <span>logo</span>
+            </div>
+       <a href="">Crear</a>
+        <a href="">Listar</a>
+        </nav>
+      </header>
+
+      <main>
+            <picture>
+                <img src="" alt="" />
+            </picture>
+            <div>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Praesentium amet aut atque laborum omnis, quis error laudantium dolorem quidem eligendi autem natus. Quibusdam atque excepturi suscipit possimus consequuntur voluptatem rerum?</p>
+            </div>
+      </main>
+    </>
+  )
+}
+
+export default App
